02493d5321204c5360767dabe580ce53c86d5e1d
justVovka <vovkamokroff@gmail.com>
2020-04-10 14:57:42 +0300

    Навигация по сайту

---
+++ src/not-found/not-found.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./not-found.sass";
+
+const NotFound = () => {
+    return (
+        <React.Fragment>
+            <h1>404</h1>
+            <h2>Страница не найдена</h2>
+        </React.Fragment>
+    );
+};
+
+export default NotFound;
